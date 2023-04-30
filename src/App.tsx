19dd831741173e9a28418c93bfa405d628b6cad6
Vladyslav Penkin <vy891078@gmail.com>
2023-04-30 20:39:08 +0300

    added solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => {
+import { Movie } from './types/Movie';
+
+export const App: React.FC = () => {
+  const [filterMovies, setFilterMovies] = useState(moviesFromServer);
+
+  const addMovieToList = (movie: Movie) => {
+    setFilterMovies(prevState => [
+      ...prevState,
+      movie,
+    ]);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={filterMovies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie onAdd={addMovieToList} />
       </div>
     </div>
   );
--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -1,56 +1,124 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TextField } from '../TextField';
 
-export const NewMovie = () => {
+import { Movie } from '../../types/Movie';
+
+type Props = {
+  onAdd: (movies: Movie) => void;
+};
+
+export const NewMovie: React.FC<Props> = ({ onAdd }) => {
   // Increase the count after successful form submission
   // to reset touched status of all the `Field`s
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [imgUrl, setImgUrl] = useState('');
+  const [imdbUrl, setImdbUrl] = useState('');
+  const [imdbId, setImdbId] = useState('');
+
+  const resetAllChanges = () => {
+    setTitle('');
+    setDescription('');
+    setImgUrl('');
+    setImdbUrl('');
+    setImdbId('');
+  };
+
+  const isEqual = (
+    title.length > 0
+    && imgUrl.length > 0
+    && imdbUrl.length > 0
+    && imdbId.length > 0
+  );
 
   return (
-    <form className="NewMovie" key={count}>
+    <form
+      className="NewMovie"
+      key={count}
+      onSubmit={(event) => {
+        event.preventDefault();
+
+        const newMovie = {
+          title,
+          description,
+          imgUrl,
+          imdbUrl,
+          imdbId,
+        };
+
+        if (isEqual) {
+          onAdd(newMovie);
+          resetAllChanges();
+          setCount(prevState => prevState + 1);
+        }
+      }}
+    >
       <h2 className="title">Add a movie</h2>
 
       <TextField
         name="title"
         label="Title"
-        value=""
-        onChange={() => {}}
+        value={title}
+        onChange={setTitle}
         required
       />
 
       <TextField
         name="description"
         label="Description"
-        value=""
+        value={description}
+        onChange={setDescription}
       />
 
       <TextField
         name="imgUrl"
         label="Image URL"
-        value=""
+        value={imgUrl}
+        onChange={setImgUrl}
+        required
       />
 
       <TextField
         name="imdbUrl"
         label="Imdb URL"
-        value=""
+        value={imdbUrl}
+        onChange={setImdbUrl}
+        required
       />
 
       <TextField
         name="imdbId"
         label="Imdb ID"
-        value=""
+        value={imdbId}
+        onChange={setImdbId}
+        required
       />
 
       <div className="field is-grouped">
         <div className="control">
-          <button
-            type="submit"
-            data-cy="submit-button"
-            className="button is-link"
-          >
-            Add
-          </button>
+          {
+            isEqual
+              ? (
+                <button
+                  type="submit"
+                  data-cy="submit-button"
+                  className="button is-link"
+                >
+                  Add
+                </button>
+              ) : (
+                <button
+                  type="submit"
+                  data-cy="submit-button"
+                  className="button is-link"
+                  disabled
+                >
+                  Add
+                </button>
+              )
+          }
         </div>
       </div>
     </form>
